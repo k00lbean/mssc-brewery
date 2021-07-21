2b4a905ca18bec59a91825bf9980b293711558c0
k00lbeanAZF <k00lbeanAZF@hotmail.com>
2021-07-21 14:45:52 -0400

    Initial commit for project

---
+++ src/test/java/com/k00lbean/msscbrewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k00lbean.msscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
