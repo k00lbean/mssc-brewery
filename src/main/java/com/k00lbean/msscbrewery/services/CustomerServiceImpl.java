d19fedf781ccb8bd1091d1254bef831c3b4df4d2
k00lbeanAZF <k00lbeanAZF@hotmail.com>
2021-07-22 17:32:30 -0400

    Added remaining Customer POST/PUT/DELETE Controller/Service routes

--- src/main/java/com/k00lbean/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/com/k00lbean/msscbrewery/services/CustomerServiceImpl.java
@@ -4,6 +4,7 @@ import com.k00lbean.msscbrewery.web.models.CustomerDto;
 
 import java.util.UUID;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -11,6 +12,7 @@ import java.util.UUID;
 /**
  * Created by dmm on 2021-07-21.
  */
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
@@ -19,5 +21,22 @@ public class CustomerServiceImpl implements CustomerService {
                 .id(UUID.randomUUID())
                 .name("Joe Buck")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //todo impl - would add a real impl to update a customer
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Delete a user...");
     }
 }
